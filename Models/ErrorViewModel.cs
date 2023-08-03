eeb2ed585af47f11cda8795995bacdd80e0fe62c
gonHid <gon.hidalgo.ch@gmail.com>
2023-08-03 13:10:11 -0400

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace repoTareas.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
